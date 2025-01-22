4369ae25fb3169ff2941eeea35687cd37f0570a2
sgordon2004 <syrrgordon@localhost>
2025-01-21 22:53:30 -0500

    First commit.

---
+++ Spring_2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Syrr Gordon
+ */
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+
+	}
+
+}

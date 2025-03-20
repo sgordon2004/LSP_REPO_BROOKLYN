3bd4bed7c81939d2405524e02be4a35d732d8ef0
sgordon2004 <syrrgordon@localhost>
2025-03-20 19:53:49 -0400

    added addCandidate and castVote methods

---
+++ Spring_2025/src/org/howard/edu/lsp/midterm/question3/VotingMachine.java
@@ -0,0 +1,43 @@
+package org.howard.edu.lsp.midterm.question3;
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * Sources:
+ * https://www.geeksforgeeks.org/map-interface-java-examples/
+ * https://www.w3schools.com/java/java_hashmap.asp
+ */
+
+public class VotingMachine {
+	// create map to store votes with candidate as key and number of votes
+	// they received as value
+	private Map<String, Integer> votes;
+	
+	// Constructor
+	public VotingMachine() {
+		votes = new HashMap<>();
+	}
+	
+	/**
+	 * This method adds a new candidate to the ballot
+	 * with 0 votes since nobody starts with a vote.
+	 * @param candidate		the political candidate on the ballot
+	 */
+	public void addCandidate(String candidate) {
+		if (!votes.containsKey(candidate)) {
+			votes.put(candidate, 0);
+		}
+	}
+	/**
+	 * This method adds one vote to the candidate if they exist.
+	 * @param candidate		the candidate being voted for
+	 * @return boolean		true if successful, false if candidate not on ballot
+	 */
+    public boolean castVote(String candidate) {
+        if (votes.containsKey(candidate)) { // if candidate exists
+            votes.put(candidate, votes.get(candidate) + 1); // incrementing the value by 1
+            return true; // Vote was successful
+        }
+        return false; // Candidate does not exist
+    }
+
+}

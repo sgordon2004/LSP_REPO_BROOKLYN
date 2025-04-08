0bc485910f672b8800a5734f6649926a98c47f21
Syrr Gordon <marcus.syrr@gmail.com>
2025-04-08 14:37:04 -0400

    Created the JUnit test file

---
+++ Spring_2025/src/org/howard/edu/lsp/assignment6/IntegerSetTest.java
@@ -0,0 +1,19 @@
+package org.howard.edu.lsp.assignment6;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class IntegerSetTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
